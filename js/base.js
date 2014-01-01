f14a60b20c80db248f86f3698a937e6591536dc6
Konboi <ryosuke.yabuki@gmail.com>
2014-01-01 12:09:13 +0000

    Added progress bar

--- js/base.js
+++ js/base.js
@@ -9,13 +9,22 @@ $(function () {
     var slides = $('.slide').length;  
     $("#contents").css("width", SLIDE_WIDTH * slides + "px");
     $(".slide").css("float", "left");
+    var progress_top = $(window).height() - 20;
+    $(".progress").css("top", progress_top + "px");
 
-
-    // slide animation
+    // next slide
     $(window).keypress(function (e) {
         if (e.which === 13 || e.which === 32 || e.which === 39) {
           current_x += SLIDE_WIDTH;
           $('body').animate({ scrollLeft: current_x, scrollTop: current_y }, 1200);
+        }
+    });
+
+    // back slide
+    $(window).keypress(function (e) {
+        if (e.which === 37 || e.which === 8) {
+          current_x -= SLIDE_WIDTH;
+          $('body').animate({scrollLeft: current_x, scrollTop: current_y}, 1200);
         }
     });
 });
