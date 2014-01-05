65e7baaf7f6ab91f57787ef70e29a3e969f5c764
Konboi <ryosuke.yabuki@gmail.com>
2014-01-05 12:45:01 +0000

    Created progreass function

--- js/base.js
+++ js/base.js
@@ -4,7 +4,7 @@ $(function () {
     var SLIDE_HEIGHT = 800;
     var current_x = 0;
     var current_y = 0;
-
+    var current_slide_number = 1;
     // initialize
     // width
     var slides = $('.slide').length;
@@ -14,12 +14,16 @@ $(function () {
     // progress bar
     var progress_top = $(window).height() - 20;
     $(".progress").css("top", progress_top + "px");
+    $("#progress-bar").css("width", (SLIDE_WIDTH/slides) * current_slide_number);
 
     // next slide
     $(window).keypress(function (e) {
         if (e.which === 13 || e.which === 32 || e.which === 39) {
           current_x += SLIDE_WIDTH;
+          current_slide_number += 1;
+
           $('body').animate({ scrollLeft: current_x, scrollTop: current_y }, 1200);
+          $("#progress-bar").css("width", (SLIDE_WIDTH/slides) * current_slide_number);
         }
     });
 
@@ -27,6 +31,7 @@ $(function () {
     $(window).keypress(function (e) {
         if (e.which === 37 || e.which === 8) {
           current_x -= SLIDE_WIDTH;
+          current_slide_number == 1;
           $('body').animate({scrollLeft: current_x, scrollTop: current_y}, 1200);
         }
     });
